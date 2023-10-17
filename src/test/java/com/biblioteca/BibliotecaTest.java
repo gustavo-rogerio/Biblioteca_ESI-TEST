47f96baa86a1a50ed8cdd006a1736636f1f443dc
LouisSiuLungLau <52263147+LouisSiuLungLau@users.noreply.github.com>
2023-10-17 11:49:11 -0300

    Update BibliotecaTest.java
    alterando detalhes

--- src/test/java/com/biblioteca/BibliotecaTest.java
+++ src/test/java/com/biblioteca/BibliotecaTest.java
@@ -9,67 +9,77 @@ public class BibliotecaTest {
 	private Biblioteca biblioteca;
 
 	@Before
-    public void setUp() { 
-      biblioteca = new Biblioteca();
-    }
+	public void setUp() { 
+		biblioteca = new Biblioteca();
+	}
 
 	@Test
-    public void testAdicionarLivro() { 
-      Livro livro = new Livro("12345", "Título do Livro", "Autor do Livro", "Editora do Livro");
-      biblioteca.AdicionarLivro(livro);
-      Assert.assertEquals(1, biblioteca.livros.size());
-    }
+	public void testAdicionarLivro() {
+		Autor autor = new Autor("Nome do Autor", "Sobrenome do Autor");
+		Editora editora = new Editora("Nome da Editora");
+		Livro livro = new Livro(autor, editora, "1234567890", "Título do Livro", 200);
+		biblioteca.AdicionarLivro(livro);
+		Assert.assertEquals(1, biblioteca.livros.size());
+	}
 
 	@Test
-    public void testRemoverLivro() {
-      Livro livro = new Livro("12345", "Título do Livro", "Autor do Livro", "Editora do Livro");
-      biblioteca.AdicionarLivro(livro);
-      biblioteca.RemoverLivro(livro);
-      Assert.assertEquals(0, biblioteca.livros.size());
-    }
+	public void testRemoverLivro() {
+		Autor autor = new Autor("Nome do Autor", "Sobrenome do Autor");
+		Editora editora = new Editora("Nome da Editora");
+		Livro livro = new Livro(autor, editora, "1234567890", "Título do Livro", 200);
+		biblioteca.AdicionarLivro(livro);
+		biblioteca.RemoverLivro(livro);
+		Assert.assertEquals(0, biblioteca.livros.size());
+	}
 
 	@Test
-    public void testAdicionarAutor() {
-      Autor autor = new Autor("Autor", "Sobrenome");
-      biblioteca.AdicionarAutor(autor);
-      Assert.assertEquals(1, biblioteca.autores.size());
-    }
+	public void testAdicionarAutor() {
+		Autor autor = new Autor("Nome do Autor", "Sobrenome do Autor");
+		biblioteca.AdicionarAutor(autor);
+		Assert.assertEquals(1, biblioteca.autores.size());
+	}
 
 	@Test
-    public void testRemoverAutor() {
-      Autor autor = new Autor("Autor", "Sobrenome");
-      biblioteca.AdicionarAutor(autor);
-      biblioteca.RemoverAutor(autor);
-      Assert.assertEquals(0, biblioteca.autores.size());
-    }
-
-    @Test
-    public void testAdicionarEditora() {
-      Editora editora = new Editora("Editora");
-      biblioteca.AdicionarEditora(editora);
-      Assert.assertEquals(1, biblioteca.editoras.size());
-    }
+	public void testRemoverAutor() {
+		Autor autor = new Autor("Nome do Autor", "Sobrenome do Autor");
+		biblioteca.AdicionarAutor(autor);
+		biblioteca.RemoverAutor(autor);
+		Assert.assertEquals(0, biblioteca.autores.size());
+	}
 
 	@Test
-    public void testRemoverEditora() {
-      Editora editora = new Editora("Editora");
-      biblioteca.AdicionarEditora(editora);
-      biblioteca.RemoverEditora(editora);
-      Assert.assertEquals(0, biblioteca.editoras.size());
-    }
+	public void testAdicionarEditora() {
+		Editora editora = new Editora("Editora");
+		biblioteca.AdicionarEditora(editora);
+		Assert.assertEquals(1, biblioteca.editoras.size());
+	}
 
-    @Test
-    public void testAdicionarEstoque() {
-      LivroEstoque livroEstoque = new LivroEstoque(new Livro("12345", "Título", "Autor", "Editora"), 10);
-      biblioteca.AdicionarEstoque(livroEstoque);
-      Assert.assertEquals(1, biblioteca.estoque.size());
-    }
+	@Test
+	public void testRemoverEditora() {
+		Editora editora = new Editora("Editora");
+		biblioteca.AdicionarEditora(editora);
+		biblioteca.RemoverEditora(editora);
+		Assert.assertEquals(0, biblioteca.editoras.size());
+		}
 
-    @Test
-    public void testRemoverEstoque() {
-      LivroEstoque livroEstoque = new LivroEstoque(new Livro("12345", "Título", "Autor", "Editora"), 10);
-      biblioteca.AdicionarEstoque(livroEstoque);
-      biblioteca.RemoverEstoque(livroEstoque);
-      Assert.assertEquals(0, biblioteca.estoque.size());
-    }
+	@Test
+	public void testAdicionarEstoque() {
+		Autor autor = new Autor("Nome do Autor", "Sobrenome do autor");
+		Editora editora = new Editora("Nome da Editora");
+		Livro livro = new Livro(autor, editora, "1234567890", "Título do Livro", 200);
+		LivroEstoque livroEstoque = new LivroEstoque(livro, 10, 50.0);
+		biblioteca.AdicionarEstoque(livroEstoque);
+		Assert.assertEquals(1, biblioteca.estoque.size());
+	}
+
+	@Test
+	public void testRemoverEstoque() {
+		Autor autor = new Autor("Nome do Autor", "Sobrenome do autor");
+		Editora editora = new Editora("Nome da Editora");
+		Livro livro = new Livro(autor, editora, "1234567890", "Título do Livro", 200);
+		LivroEstoque livroEstoque = new LivroEstoque(livro, 10, 50.0);
+		biblioteca.AdicionarEstoque(livroEstoque);
+		biblioteca.RemoverEstoque(livroEstoque);
+		Assert.assertEquals(0, biblioteca.estoque.size());
+	}
 }
